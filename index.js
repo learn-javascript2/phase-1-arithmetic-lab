adedcaf4dbab20fee5c2f7bc4e71d34ef343217a
andyjohn23 <andrewnick906@gmail.com>
2024-03-30 21:59:37 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,56 @@
-
 // Write your code here
+// let myVar;
+
+// if (myVar == null) {
+//     console.log("Value either undefined or null.")
+// } else {
+//     console.log("Value neither undefined nor null.")
+// }
+
+// let myVar;
+
+// if (myVar === undefined) {
+//     console.log("value is undefined.")
+// } else if (myVar === null) {
+//     console.log("value is null.")
+// } else {
+//     console.log("value is neither undefined nor null.")
+// }
+
+// let myVar;
+
+// if (typeof myVar === "undefined") {
+//     console.log("value is undefined.")
+// } else if (myVar === null) {
+//     console.log("value is null.")
+// } else {
+//     console.log("value is neither undefined nor null.")
+// }
+
+// function isNullorUndefined(value) {
+//     return value === undefined || value === null;
+// }
+
+// let myVar;
+
+// if (isNullorUndefined(myVar)) {
+//     console.log("value is either undefined or null.")
+// } else {
+//     console.log("value is neither undefined nor null.")
+// }
+
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply);
+
+let random = Math.floor(Math.random() * 10) + 1;
+console.log(random);
+
+let num3 = 22;
+let num4 = 9;
+let mod = num3 % num4;
+console.log(mod);
+
+let max = Math.max(2, 20, 10, 4, 8)
+console.log(max);
